42fc76a6f7ebf27319a83b105f6bfaf1977f1531
Francis Avila <favila@dancingmammoth.com>
2013-04-12 17:17:59 -0500

    Improve lookup table generation
    Strips strings and skips lines without wikipedia records

--- scripts/wikilookup.py
+++ scripts/wikilookup.py
@@ -20,11 +20,11 @@ def convertfile(infile, outfile):
         csvreader = csv.reader(ifp)
         csvreader.next()
         for row in csvreader:
-            row = [i.decode('CP1252') for i in row]
-            ET.SubElement(root, 'item', id=row[0],
-                          name=row[1],
-                          wikipedia=canonicalize_wiki_url(
-                              row[2]))
+            row = [i.decode('CP1252').strip() for i in row]
+            if all(row[:3]):
+                id, name = row[:2]
+                wikipedia = canonicalize_wiki_url(row[2])
+                ET.SubElement(root, 'item', id=id, name=name, wikipedia=wikipedia)
     ET.ElementTree(root).write(outfile, 'utf-8')
 
 if __name__ == '__main__':
